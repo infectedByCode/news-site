f54a79da788c01921b4f86d42b79df021458de1d
Matt Daley <mdaleyhk@gmail.com>
2019-10-28 16:46:49 +0000

    Add /api/topics in the topicRouter. Minor change to apiRouter for path.

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2,5 +2,9 @@ const apiRouter = require('express').Router();
 const topicRouter = require('./topicRouter');
 
 apiRouter.use('/topics', topicRouter);
+// Refactor to include in error folder
+apiRouter.get('/*', (req, res, next) => {
+  res.status(404).send({ msg: 'Error 404 - Invalid URL provided.' });
+});
 
 module.exports = apiRouter;
