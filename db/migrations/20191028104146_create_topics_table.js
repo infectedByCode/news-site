eb8af300fa93ef36440d53f85d8f7ad59da1b977
Matt Daley <mdaleyhk@gmail.com>
2019-10-28 10:57:24 +0000

    Config topics table creation.

--- db/migrations/20191028104146_create_topics_table.js
+++ db/migrations/20191028104146_create_topics_table.js
@@ -1,8 +1,10 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('topics', topicsTable => {
+    topicsTable.uuid('slug').primary();
+    topicsTable.string('description').notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('topics');
 };
