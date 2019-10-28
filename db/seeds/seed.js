122c9f40fd6da0fde1e45ebcf129dfce90bcb3a3
Matt Daley <mdaleyhk@gmail.com>
2019-10-28 15:12:00 +0000

    Complete seed file

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -16,36 +16,19 @@ exports.seed = function(knex) {
 
       return Promise.all([topicsInsertions, usersInsertions]);
     })
-    .then(([topicsInsertions, usersInsertions]) => {
+    .then(() => {
       const modifiedArticleData = formatDates(articleData);
 
-      return knex('articles')
+      return (articlesInsertions = knex('articles')
         .join('topics', 'articles.topic', 'topics.slug')
         .join('users', 'articles.author', 'users.username')
-        .insert(modifiedArticleData);
+        .insert(modifiedArticleData)
+        .returning('*'));
+    })
+    .then(articleRows => {
+      const articleIdRef = makeRefObj(articleRows);
+      const formattedComments = formatComments(commentData, articleIdRef);
+
+      return knex('comments').insert(formattedComments);
     });
-
-  //     /*
-  //     yyyy-mm-dd hh-mm-ss
-  //     Your article data is currently in the incorrect format and will violate your SQL schema.
-
-  //     You will need to write and test the provided formatDate utility function to be able insert your article data.
-
-  //     Your comment insertions will depend on information from the seeded articles, so make sure to return the data after it's been seeded.
-  //     */
-  //   })
-  //   .then(articleRows => {
-  //     /*
-
-  //     Your comment data is currently in the incorrect format and will violate your SQL schema.
-
-  //     Keys need renaming, values need changing, and most annoyingly, your comments currently only refer to the title of the article they belong to, not the id.
-
-  //     You will need to write and test the provided makeRefObj and formatComments utility functions to be able insert your comment data.
-  //     */
-
-  //     const articleRef = makeRefObj(articleRows);
-  //     const formattedComments = formatComments(commentData, articleRef);
-  //     return knex('comments').insert(formattedComments);
-  //   });
 };
