69ef280dfb5ccef58e56bcacbebe428dfda97877
Matt Daley <mdaleyhk@gmail.com>
2019-10-28 11:38:28 +0000

    Create schema for comments table

--- db/migrations/20191028112821_create_comments_table.js
+++ db/migrations/20191028112821_create_comments_table.js
@@ -1,8 +1,20 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('comments', commentsTable => {
+    commentsTable.increments('comment_id').primary();
+    commentsTable
+      .string('author')
+      .references('users.username')
+      .notNullable();
+    commentsTable
+      .integer('article_id')
+      .references('articles.id')
+      .notNullable();
+    commentsTable.integer('votes').defaultTo(0);
+    commentsTable.timestamp('created_at').defaultTo(knex.fn.now());
+    commentsTable.string('body').notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('comments');
 };
