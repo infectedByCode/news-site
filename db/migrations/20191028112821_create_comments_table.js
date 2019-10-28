e018f2716fbe1a58e45ed276ba0a1ac192216f55
Matt Daley <mdaleyhk@gmail.com>
2019-10-28 13:54:19 +0000

    Changed body to text from string

--- db/migrations/20191028112821_create_comments_table.js
+++ db/migrations/20191028112821_create_comments_table.js
@@ -11,7 +11,7 @@ exports.up = function(knex) {
       .notNullable();
     commentsTable.integer('votes').defaultTo(0);
     commentsTable.timestamp('created_at').defaultTo(knex.fn.now());
-    commentsTable.string('body').notNullable();
+    commentsTable.text('body').notNullable();
   });
 };
 
