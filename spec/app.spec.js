cad6f4c0f878c0bd47bcaf4c97a9628e7c060952
Matt Daley <mdaleyhk@gmail.com>
2019-10-28 16:45:52 +0000

    Write tests for /api error and /api/topics route.

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,18 +1,44 @@
 process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
-const { expect } = chai.expect;
+const expect = chai.expect;
 const request = require('supertest');
 
 const app = require('../app');
+const connection = require('../db/connection');
 
 describe('app.js', () => {
   // Setup for tests
   beforeEach(() => {
     return connection.seed.run();
   });
-  afterEach(() => {
-    connection.destroy();
+  after(() => {
+    return connection.destroy();
   });
   // Testing for endpoints
+  describe('/api', () => {
+    describe('/api/topics', () => {
+      it('GET:200, returns an object with all topics in the database.', () => {
+        return request(app)
+          .get('/api/topics')
+          .expect(200)
+          .then(({ body: { topics } }) => {
+            expect(topics).to.have.lengthOf(3);
+            topics.forEach(topic => {
+              expect(topic).to.have.keys(['slug', 'description']);
+            });
+          });
+      });
+    });
+  });
+  describe('/api ERRORS', () => {
+    it('GET:404, when URL is invalid enpoint ', () => {
+      return request(app)
+        .get('/api/sdada')
+        .expect(404)
+        .then(({ body: { msg } }) => {
+          expect(msg).to.equal('Error 404 - Invalid URL provided.');
+        });
+    });
+  });
 });
