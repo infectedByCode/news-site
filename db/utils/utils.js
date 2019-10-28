946dade2844d66b0f845a90cfa2d41e9115f2059
Matt Daley <mdaleyhk@gmail.com>
2019-10-28 12:46:27 +0000

    Code formatDates() util function to reformat dates

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,15 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  const dataArr = list.map(item => {
+    const newTimestamp = new Date(item.created_at);
+    const newObj = {
+      ...item
+    };
+    newObj.created_at = newTimestamp;
+
+    return newObj;
+  });
+  return dataArr;
+};
 
 exports.makeRefObj = list => {};
 
