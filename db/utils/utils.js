1d626252014984ad1c13657fc3bb40e0c597febc
Matt Daley <mdaleyhk@gmail.com>
2019-10-28 15:02:32 +0000

    Code functions to assist with comments reformat.

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -11,6 +11,33 @@ exports.formatDates = list => {
   return dataArr;
 };
 
-exports.makeRefObj = list => {};
+exports.makeRefObj = list => {
+  const articleObjRef = {};
 
-exports.formatComments = (comments, articleRef) => {};
+  list.forEach(article => {
+    const { id, title } = article;
+    articleObjRef[title] = id;
+  });
+
+  return articleObjRef;
+};
+
+exports.formatComments = (comments, articleRef) => {
+  const newCommentArr = comments.map(comment => {
+    const newTimestamp = new Date(comment.created_at);
+    const { body, votes, created_at, belongs_to } = comment;
+    const article_id = articleRef[belongs_to];
+
+    const newComment = {
+      article_id,
+      author: comment.created_by,
+      body,
+      votes,
+      created_at
+    };
+    newComment.created_at = newTimestamp;
+
+    return newComment;
+  });
+  return newCommentArr;
+};
