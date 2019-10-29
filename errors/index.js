30d3a747f6b68ec91493c5e5f71802778c215679
Matt Daley <mdaleyhk@gmail.com>
2019-10-29 12:09:03 +0000

    Update errors to include PSQL errors.

--- errors/index.js
+++ errors/index.js
@@ -3,3 +3,21 @@ exports.customErrorHandlers = (err, req, res, next) => {
     res.status(err.status).send({ msg: err.msg });
   } else next(err);
 };
+
+exports.psqlErrorHandlers = (err, req, res, next) => {
+  const errRef = {
+    '22P02': {
+      status: 400,
+      msg: createErrorMessage(err)
+    }
+  };
+
+  if (err.code) {
+    res.status(errRef[err.code].status).send({ msg: errRef[err.code].msg });
+  }
+};
+
+const createErrorMessage = err => {
+  const msg = err.message.split(' - ')[1];
+  return msg;
+};
