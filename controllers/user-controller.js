0c17b4b20ee835fec120393be5139e3134ae1a83
Matt Daley <mdaleyhk@gmail.com>
2019-10-29 10:18:42 +0000

    Create contoller for users.

---
+++ controllers/user-controller.js
@@ -0,0 +1,10 @@
+const { selectUserByUsername } = require('../models/user-models');
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;
+  selectUserByUsername(username)
+    .then(user => {
+      res.status(200).send({ user });
+    })
+    .catch();
+};
