c62942017c1ac9227b1f0c6ce5b850baf908abf9
Matt Daley <mdaleyhk@gmail.com>
2019-10-29 13:11:19 +0000

    Add error handling

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routes/apiRouter');
-const { customErrorHandlers } = require('./errors');
+const { customErrorHandlers, psqlErrorHandlers } = require('./errors');
 
 app.use(express.json());
 
 app.use('/api', apiRouter);
 app.use(customErrorHandlers);
+app.use(psqlErrorHandlers);
 
 module.exports = app;
