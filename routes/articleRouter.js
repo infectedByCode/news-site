ff730a2564e354b773af21c43c73a6eb98966187
Matt Daley <mdaleyhk@gmail.com>
2019-10-29 12:16:09 +0000

    Update router to include patchArticleById

--- routes/articleRouter.js
+++ routes/articleRouter.js
@@ -1,6 +1,9 @@
 const articleRouter = require('express').Router();
-const { getArticleById } = require('../controllers/article-controller');
+const { getArticleById, patchArticleById } = require('../controllers/article-controller');
 
-articleRouter.route('/:article_id').get(getArticleById);
+articleRouter
+  .route('/:article_id')
+  .get(getArticleById)
+  .patch(patchArticleById);
 
 module.exports = articleRouter;
