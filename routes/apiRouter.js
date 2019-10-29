e5207338549cdabaf0c0397011cb95cbea76d73c
Matt Daley <mdaleyhk@gmail.com>
2019-10-29 10:46:00 +0000

    Add articleRouter into apiRouter.

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,9 +1,11 @@
 const apiRouter = require('express').Router();
 const topicRouter = require('./topicRouter');
 const userRouter = require('./userRouter');
+const articleRouter = require('./articleRouter');
 
 apiRouter.use('/topics', topicRouter);
 apiRouter.use('/users', userRouter);
+apiRouter.use('/articles', articleRouter);
 
 apiRouter.get('/*', (req, res, next) => {
   res.status(404).send({ msg: 'Error 404 - Invalid URL provided.' });
