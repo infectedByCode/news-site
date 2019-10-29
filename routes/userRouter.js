1ce653da90a0153b74ab7d90fa91ccc62dcd2e66
Matt Daley <mdaleyhk@gmail.com>
2019-10-29 10:19:53 +0000

    Rename controller being passed into.

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,6 @@
 const userRouter = require('express').Router();
+const { getUserByUsername } = require('../controllers/user-controller');
 
-userRouter.route('/:username').get(getUserById);
+userRouter.route('/:username').get(getUserByUsername);
 
 module.exports = userRouter;
