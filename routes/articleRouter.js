7e289f9416c1f310e8c7f1efd4b1a3cedf232cf2
Matt Daley <mdaleyhk@gmail.com>
2019-10-29 10:55:37 +0000

    Fix typo on router

--- routes/articleRouter.js
+++ routes/articleRouter.js
@@ -1,6 +1,6 @@
 const articleRouter = require('express').Router();
 const { getArticleById } = require('../controllers/article-controller');
 
-articleRouter.route('/:article_id', getArticleById);
+articleRouter.route('/:article_id').get(getArticleById);
 
 module.exports = articleRouter;
