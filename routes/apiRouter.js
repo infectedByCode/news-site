98b78edb46c5c427e60a7b975cb0a31978d9716c
Matt Daley <mdaleyhk@gmail.com>
2019-10-29 10:02:52 +0000

    Setup router for users.

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,8 +1,10 @@
 const apiRouter = require('express').Router();
 const topicRouter = require('./topicRouter');
+const userRouter = require('./userRouter');
 
 apiRouter.use('/topics', topicRouter);
-// Refactor to include in error folder
+apiRouter.use('/users', userRouter);
+
 apiRouter.get('/*', (req, res, next) => {
   res.status(404).send({ msg: 'Error 404 - Invalid URL provided.' });
 });
