0fa13950646a0b968615e8b96ae4e4f018fd4b6f
Matt Daley <mdaleyhk@gmail.com>
2019-10-30 14:40:25 +0000

    Handle valid comment ID not found 404.

--- models/comment-models.js
+++ models/comment-models.js
@@ -4,5 +4,9 @@ exports.updateCommentById = (comment_id, votesUpdate) => {
   return connection('comments')
     .where('comment_id', '=', comment_id)
     .increment('votes', votesUpdate)
-    .returning('*');
+    .returning('*')
+    .then(comment => {
+      if (!comment.length) return Promise.reject({ status: 404, msg: `Comment "${comment_id}" cannot be found.` });
+      else return Promise.resolve(comment);
+    });
 };
