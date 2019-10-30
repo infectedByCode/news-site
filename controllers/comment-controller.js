027841016943d3ed9b31f4d25b3178e2a9636b80
Matt Daley <mdaleyhk@gmail.com>
2019-10-30 14:54:53 +0000

    Implement logic for error handling for update comment by ID.

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -2,9 +2,9 @@ const { updateCommentById } = require('../models/comment-models');
 
 exports.patchCommentById = (req, res, next) => {
   const { comment_id } = req.params;
-  const { inc_votes } = req.body;
+  const { inc_votes, ...otherData } = req.body;
 
-  updateCommentById(comment_id, inc_votes)
+  updateCommentById(comment_id, inc_votes, otherData)
     .then(([comment]) => {
       res.status(200).send({ comment });
     })
--- models/comment-models.js
+++ models/comment-models.js
@@ -1,6 +1,9 @@
 const connection = require('../db/connection');
 
-exports.updateCommentById = (comment_id, votesUpdate) => {
+exports.updateCommentById = (comment_id, votesUpdate, otherData) => {
+  if (Object.keys(otherData).length)
+    return Promise.reject({ status: 400, msg: 'Please only include { inc_votes: "number" } when using the API' });
+
   return connection('comments')
     .where('comment_id', '=', comment_id)
     .increment('votes', votesUpdate)
