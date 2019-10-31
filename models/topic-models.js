2d2ae41f9f08fecaa5ce6d26301845a21c56a606
Matt Daley <mdaleyhk@gmail.com>
2019-10-31 15:13:36 +0000

    remove limit and page query as to trouble deployment issue.

--- models/topic-models.js
+++ models/topic-models.js
@@ -1,9 +1,7 @@
 const connection = require('../db/connection');
 
 exports.selectTopics = (limit = 5, p = 1) => {
-  return connection
-    .select('*')
-    .from('topics')
-    .limit(limit)
-    .offset(limit * (p - 1));
+  return connection.select('*').from('topics');
+  // .limit(limit)
+  // .offset(limit * (p - 1));
 };
