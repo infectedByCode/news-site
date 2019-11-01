3f4b6e8f25a95d54335d75bdfbabf5ddad59acfd
Matt Daley <mdaleyhk@gmail.com>
2019-11-01 12:11:01 +0000

    Add topic data with a multiple word slug.

--- db/data/test-data/topics.js
+++ db/data/test-data/topics.js
@@ -1,14 +1,18 @@
 module.exports = [
   {
     description: 'The man, the Mitch, the legend',
-    slug: 'mitch',
+    slug: 'mitch'
   },
   {
     description: 'Not dogs',
-    slug: 'cats',
+    slug: 'cats'
   },
   {
     description: 'what books are made of',
-    slug: 'paper',
+    slug: 'paper'
   },
+  {
+    description: 'A topic for Hong Kong',
+    slug: 'Hong Kong'
+  }
 ];
