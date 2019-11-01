b1eca19c20cee05f587a94a82e3d09cf9f41af58
Matt Daley <mdaleyhk@gmail.com>
2019-11-01 11:57:43 +0000

    Revert changes as tests were failing. Implement testing for multiple words and case.

--- models/articles-models.js
+++ models/articles-models.js
@@ -92,7 +92,7 @@ exports.selectArticles = (sort_by = 'created_at', order = 'desc', author, topic,
     .modify(query => {
       if (author && topic) query.where('articles.author', author).andWhere('articles.topic', topic);
       if (author) query.where('articles.author', author);
-      if (topic) query.where('articles.topic', 'like', `%${topic}%`);
+      if (topic) query.where('articles.topic', topic);
     })
     .modify(query => {
       if (limit || p) query.limit(limit).offset(limit * (p - 1));
@@ -112,7 +112,7 @@ exports.selectArticles = (sort_by = 'created_at', order = 'desc', author, topic,
       if (!query.length && topic) {
         return connection('topics')
           .select('*')
-          .where('topics.slug', 'like', `%${topic}$`)
+          .where('topics.slug', '=', topic)
           .then(query => {
             if (!query.length) return Promise.reject({ status: 404, msg: `Topic "${topic}" does not exist.` });
             else return Promise.resolve([]);
