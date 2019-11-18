b82564d2c5864ce21fb7c0994a9452beae710ca3
Matt Daley <mdaleyhk@gmail.com>
2019-11-18 10:35:19 +0000

    Add cors support

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const app = express();
-const apiRouter = require('./routes/apiRouter');
-const { customErrorHandlers, psqlErrorHandlers, invalidURLError, handleServerErrors } = require('./errors');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const apiRouter = require('./routes/apiRouter')
+const { customErrorHandlers, psqlErrorHandlers, invalidURLError, handleServerErrors } = require('./errors')
 
-app.use(express.json());
+app.use(
+  cors({
+    origin: 'https://msd-news.herokuapp.com',
+    credentials: true
+  })
+)
+app.use(express.json())
 
-app.use('/api', apiRouter);
-app.all('/*', invalidURLError);
+app.use('/api', apiRouter)
+app.all('/*', invalidURLError)
 
-app.use(customErrorHandlers);
-app.use(psqlErrorHandlers);
-app.use(handleServerErrors);
+app.use(customErrorHandlers)
+app.use(psqlErrorHandlers)
+app.use(handleServerErrors)
 
-module.exports = app;
+module.exports = app
